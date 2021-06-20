916ebb7f0568850b8503a7c862f42a995cf2d4ed
deryacakmak <36774966+deryacakmak@users.noreply.github.com>
2021-06-20 22:05:48 +0300

    creating table

--- gazete-dergi-otomasyon/src/main/java/com/gazeteDergiOtomasyon/model/User.java
+++ gazete-dergi-otomasyon/src/main/java/com/gazeteDergiOtomasyon/model/User.java
@@ -1,26 +1,59 @@
 package com.gazeteDergiOtomasyon.model;
 
-
-
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
-@Table(name="USER")
-public class User implements Serializable {
-
-    private static final long serialVersionUID = 6805770934683941123L;
-
-	@Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="ID", unique = true, nullable = false)
-    private Integer id;
-
-    @Column(name="NAME", unique = true, nullable = false)
-    private String name;
-
-    @Column(name="SURNAME", unique = true, nullable = false)
-    private String surname;
-
-
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "dob")
+    private String dob;
+    public User() {
+    }
+    public User(int id, String firstName, String lastName, String email, String dob) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dob = dob;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDob() {
+        return dob;
+    }
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
 }
